13e630743216aa0d0d9d00f24e427d87a3cef58b
Paweł <pawel.musial@microsoft.wsei.edu.pl>
2024-10-29 14:15:31 +0100

    Podłączenie na laptop firmowy

---
+++ Migrations/20241029131438_name1praca.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sellasist_Optima.Migrations
+{
+    /// <inheritdoc />
+    public partial class name1praca : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
